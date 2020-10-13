37b3f68bad6747d7ed02fa2219e5ba0b9b3ccaa9
srikanthpragada <srikanthpragada@gmail.com>
2020-10-13 21:57:41 +0530

    13-OCT

---
+++ Numbers.java
@@ -0,0 +1,16 @@
+
+
+// Program to print numbers from 1 to 10
+
+public class Numbers 
+{
+	
+	public static void main(String args[])
+	{
+      int i;
+
+           for(i=1; i <= 10; i ++)
+                System.out.println(i);
+   
+	}
+}
---
+++ upload.bat
@@ -0,0 +1,10 @@
+rem add all files
+git add .
+
+rem commit with the given message 
+git commit -m %1
+
+rem push main branch of this remote project
+git push javase_8_oct_2020 main
+
+
