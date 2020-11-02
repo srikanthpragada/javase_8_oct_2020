a597493a5c05a9c6db0991ccb5ff61b3fa73806e
srikanthpragada <srikanthpragada@gmail.com>
2020-11-02 18:55:03 +0530

    02-NOV

---
+++ demo/src/exhand/CheckedDemo.java
@@ -0,0 +1,9 @@
+package exhand;
+
+public class CheckedDemo {
+	public static void main(String[] args) throws InterruptedException {
+		int n = Integer.parseInt("abc");
+
+		Thread.sleep(1000);
+	}
+}
--- demo/src/oop/TestTime.java
+++ demo/src/oop/TestTime.java
@@ -2,13 +2,31 @@ package oop;
 
 import java.util.Arrays;
 
+class InvalidTimeException extends Exception {
+	public InvalidTimeException(String message) {
+		super(message);
+	}
+}
+
 class Time implements Comparable<Time> {
 	private int h, m, s;
 
-	public Time(int h, int m, int s) {
-		this.h = h;
+	public Time(int h, int m, int s) throws InvalidTimeException {
+		if (h >= 0 && h <= 23)
+			this.h = h;
+		else
+			throw new InvalidTimeException("Invalid hour!");
+
 		this.m = m;
-		this.s = s;
+		if (m >= 0 && m <= 59)
+			this.m = m;
+		else
+			throw new InvalidTimeException("Invalid minutes!");
+
+		if (s >= 0 && s <= 59)
+			this.s = s;
+		else
+			throw new InvalidTimeException("Invalid seconds!");
 	}
 
 	@Override
@@ -22,18 +40,25 @@ class Time implements Comparable<Time> {
 
 	@Override
 	public int compareTo(Time other) {
-		 return this.totalSeconds() - other.totalSeconds(); 
+		return this.totalSeconds() - other.totalSeconds();
 	}
 }
 
 public class TestTime {
 	public static void main(String[] args) {
-		Time[] times = { new Time(1, 1, 1), new Time(10, 20, 30), new Time(1, 5, 10), new Time(20, 20, 20),
-				new Time(10, 10, 10) };
+		try {
+			Time[] times = { new Time(1, 1, 1), 
+					         new Time(10, 20, 30), 
+					         new Time(1, 5, 10), 
+					         new Time(20, 20, 20),
+					         new Time(30, 10, 10) }; 
 
-		Arrays.sort(times);  // uses compareTo() 
+			Arrays.sort(times); // uses compareTo()
 
-		for (Time t : times)
-			System.out.println(t); // t.toString()
+			for (Time t : times)
+				System.out.println(t); // t.toString()
+		} catch (InvalidTimeException ex) {
+			System.out.println(ex.getMessage());
+		}
 	}
 }
