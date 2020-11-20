aa1246abe4b8d8afe7c9dc50b82217a9573fd049
srikanthpragada <srikanthpragada@gmail.com>
2020-11-20 18:54:27 +0530

    20-NOV

--- demo/src/swing/PrimeFrame.java
+++ demo/src/swing/PrimeFrame.java
@@ -7,6 +7,7 @@ import java.awt.Container;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -22,8 +23,25 @@ public class PrimeFrame extends JFrame {
 		tfNumber = new JTextField(20);
 		btnPrime = new JButton("Prime");
 		btnPrime.setBackground(Color.lightGray);
+
+		btnPrime.addActionListener(e -> {
+			int num = Integer.parseInt(tfNumber.getText());
+			boolean prime = true;
+			for (int i = 2; i <= num / 2; i++) {
+				if (num % i == 0) {
+					prime = false;
+					break;
+				}
+			}
+			if (prime)
+				JOptionPane.showMessageDialog(this, "Prime number!");
+			else
+				JOptionPane.showMessageDialog(this, "Not a Prime number!");
+		});
+
 		btnExit = new JButton("Exit");
 		btnExit.setBackground(Color.lightGray);
+		btnExit.addActionListener(e -> System.exit(0));
 
 		JPanel tp = new JPanel();
 		tp.add(new JLabel("Number :"));
@@ -45,5 +63,4 @@ public class PrimeFrame extends JFrame {
 	public static void main(String[] args) {
 		new PrimeFrame();
 	}
-
 }
